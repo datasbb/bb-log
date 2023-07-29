9682fe143e1883d6f5470f56d535aeb8c6423265
Bobae Lee <46547329+datasbb@users.noreply.github.com>
2023-07-29 20:05:41 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "bblog",
+    name: "morethanmin",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "IT Technology Study",
-    bio: "DA",
-    email: "leeobw@gmail.com",
-    linkedin: "",
-    github: "",
+    role: "frontend developer",
+    bio: "I develop everything using node.",
+    email: "morethanmin.dev@gmail.com",
+    linkedin: "morethanmin",
+    github: "morethanmin",
     instagram: "",
   },
   projects: [
     {
-      name: `bb-log`,
-      href: "https://github.com/datasbb/bb-log",
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "bb-log",
-    description: "welcome to bb-log!",
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://bb-log.vercel.app",
-  since: 2023, // If leave this empty, current year will be used.
+  link: "https://morethan-log.vercel.app",
+  since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
@@ -47,9 +47,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: false,
+    enable: true,
     config: {
-      repo: "datasbb/bb-log",
+      repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
